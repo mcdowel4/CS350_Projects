a3b0a89bec90c4f81362331e4e85444a7391ec12
mcdowel4 <david.mcdowell@siu.edu>
2019-08-02 14:31:02 -0500

    Initing

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,5 +1,5 @@
 function displayThankYou() {
-    if(window.confirm("Click Yes to submit..."))
+    if(window.confirm("Click OK to submit..."))
     {
         alert("Thank you…. a confirmation email message will be sent to you soon. This is a test");
     }

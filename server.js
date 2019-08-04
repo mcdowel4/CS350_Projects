afc565cc75ba8ccfcd153021fa6d85c5be3dc7cb
mcdowel4 <david.mcdowell@siu.edu>
2019-08-04 12:13:57 -0500

    Initing

--- server.js
+++ server.js
@@ -5,19 +5,8 @@ const port = process.env.PORT || 8080;
 const nodeoutlook = require('nodejs-nodemailer-outlook')
 const bodyParser = require('body-parser');
 const path = require('path');
-const mong = require('mongodb').MongoClient;
-const url = 'https://vast-hamlet-44994.herokuapp.com/';
-
-mongo.connect(url, (err, client) => {
-    if(err){
-        console.error(err);
-        return;
-    }
-});
-
-const db = client.db('kennel');
-
-const collection = db.collection('feedback');
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://heroku_4zw6ftcc:<dbpassword>@ds217452.mlab.com:17452/heroku_4zw6ftcc';
 
 app.use(express.static(__dirname));
 
@@ -35,11 +24,40 @@ app.listen(port, function()
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-collection.insertMany([{name:'name'}, {name:'address'}, {name:'phone'}, {name:'email'}, {name:'comment'}], (err, result) =>
-{
-    
+
+
+app.post('/mail', (req, res) => {
+    mongo.connect(url, (err, client) => {
+        if(err){
+            console.error(err);
+            return;
+        };
+        var db = client.db('heroku_4zw6ftcc');
+
+        db.collection('feedback_data').insertOne(req.body, function(err)
+        {
+            if(err) res.redirect('summer_schedule.html');
+            var numFeedback = db.collection('feedback_data').countDocuments({});
+
+            nodeoutlook.sendEmail({
+                auth:{
+                    user: "david.mcdowell.CS350@outlook.com",
+                    pass: "david.mcdowell.password"
+                }, 
+                from: 'david.mcdowell.CS350@outlook.com',
+                to: req.body.email,
+                subject: 'Thank You for your submission',
+                html: `<b>You are the ${numFeedback}</b>`,
+                text: 'Is this in the body?',
+        
+                onError: (e) => res.redirect('summer_schedule.html'),
+                onSuccess: (i) => res.redirect('index.html')       
+            });
+        })
+    });
 })
 
+/*
 app.post('/mail', (req, res) => {
     console.log(req.body);
 
@@ -58,3 +76,4 @@ app.post('/mail', (req, res) => {
         onSuccess: (i) => res.redirect('index.html')       
     });
 });
+*/

9734afadcd30f733f9fcaa0f600a30729b35955e
mcdowel4 <david.mcdowell@siu.edu>
2019-08-04 22:40:53 -0500

    Initing

--- server.js
+++ server.js
@@ -4,27 +4,26 @@ const app = express();
 const port = process.env.PORT || 8080;
 const nodeoutlook = require('nodejs-nodemailer-outlook')
 const bodyParser = require('body-parser');
-const path = require('path');
-const mongo = require('mongodb').MongoClient;
-const url = 'mongodb://heroku_4zw6ftcc:54gcq4gu7etb3lv4cn62bnfuvj@ds217452.mlab.com:17452/heroku_4zw6ftcc';
 
 app.use(express.static(__dirname));
 
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://heroku_4zw6ftcc:54gcq4gu7etb3lv4cn62bnfuvj@ds217452.mlab.com:17452/heroku_4zw6ftcc';
+
+
+//Mailer
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.get("/", function(req, res)
 {
-    res.render("index");
+    res.send("index.html");
 });
 
 app.listen(port, function()
 {
     console.log("app running");
 });
-
-//Mailer
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-
-
 
 app.post('/mail', function(req, res){
     mongo.connect(url, {useNewUrlParser: true },function(err, client){
@@ -36,26 +35,22 @@ app.post('/mail', function(req, res){
         {
             if(err) throw err;
             db.collection('feedback_data').countDocuments({}).then(function(numItems){
-                console.log(numItems);
-                /*nodeoutlook.sendEmail({
+                nodeoutlook.sendEmail({
                     auth:{
                         user: "david.mcdowell@siu.edu",
                         pass: "Lassally00135???l"
                     }, 
-                    from: 'david.mcdowell@siu.edu',
+                    from: `david.mcdowell@siu.edu`,
                     to: req.body.email,
-                    subject: 'Thank You for your submission',
+                    subject: `Thank You for your submission`,
                     html: `<b>You are the ${numItems} </b>`,
-                    text: 'Is this in the body?',
-            
-                    onError: (e) => res.redirect('summer_schedule.html'),
-                    onSuccess: (i) => res.redirect('index.html')       
-                });*/
+                    text: 'Is this in the body?'       
+                });
             });
         });
     });
 });
-
+/*
 app.post('/mail', (req, res) => {
     console.log(req.body);
 
@@ -75,3 +70,4 @@ app.post('/mail', (req, res) => {
     });
 });
 
+*/
